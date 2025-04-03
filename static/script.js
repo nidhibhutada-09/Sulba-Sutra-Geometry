ab5485c78db6c881587574ad32af00c7386310da
vboxuser <vboxuser@Ubuntu22.myguest.virtualbox.org>
2025-04-03 06:26:37 +0530

    Shloka

---
+++ static/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.classList.add("loaded");
+    document.body.style.visibility = "visible";
+    document.body.style.opacity = "1";
+});
